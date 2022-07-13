b90dce8752e820249abca7ab6da9814d4b42840a
Igor Alves <igor.brito.alves@outlook.com.br>
2022-07-13 20:45:54 -0300

    regras de negócio 1 e 2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,51 @@
-const { response } = require('express');
+const { response, request } = require('express');
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+const customers = [];
 
 app.use(express.json());
 
 app.listen(3333);
 
-app.get("/courses", (req, res) => {
-    return res.json([ "Curso 1", "Curso 2", "Curso 3" ]);
+app.get("/statement", (req, res) => {
+    const { cpf } = req.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+    
+    if(!customer) {
+        return res.status(400).json({error: "Customer not found."})
+    }
+
+    return res.json(customer.statement);
 });
 
-app.post("/courses", (req, res) => {
-    return res.json([ "Curso 1", "Curso 2", "Curso 3", "Curso 4" ])
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body;
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if(customerAlreadyExists) {
+        return res.status(400).json({error: "Customer already exists!"});
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: [],
+    });
+    return res.status(201).send();
 });
 
 app.put("/courses/:id", (req, res) => {
