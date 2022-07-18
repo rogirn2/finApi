b420818ed4762872dd719f776709c7554a7e5db9
Igor Alves <igor.brito.alves@outlook.com.br>
2022-07-18 20:24:11 -0300

    finalizando requisitos e regras de negócio

--- src/index.js
+++ src/index.js
@@ -6,28 +6,39 @@ const app = express();
 
 const customers = [];
 
-app.use(express.json());
-
-app.listen(3333);
-
-app.get("/statement", (req, res) => {
+//Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
     const { cpf } = req.headers;
 
     const customer = customers.find(customer => customer.cpf === cpf);
-    
+
     if(!customer) {
         return res.status(400).json({error: "Customer not found."})
     }
 
-    return res.json(customer.statement);
-});
+    req.customer = customer;
 
-/**
- * cpf - string
- * name - string
- * id - uuid
- * statement []
- */
+    return next();
+}
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit') {
+            return acc + operation.amount;
+        }
+
+        return acc - operation.amount;
+    }, 0);
+    
+    return balance;
+}
+
+app.use(express.json());
+
+app.listen(3333);
+
+// app.use(verifyIfExistsAccountCPF);
+
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body;
 
@@ -48,14 +59,92 @@ app.post("/account", (req, res) => {
     return res.status(201).send();
 });
 
-app.put("/courses/:id", (req, res) => {
-    return res.json([ "Curso Alterado", "Curso 2", "Curso 3", "Curso 4" ])
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req;
+    
+    return res.json(customer.statement);
 });
 
-app.patch("/courses/:id", (req, res) => {
-    return res.json([ "Curso Alterado", "Curso Patch", "Curso 3", "Curso 4" ])
+app.get("/statement/date", verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req;
+    const { date } = req.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    const statement = customer.statement.filter(
+        (statement) => 
+            statement.created_at.toDateString() === 
+            new Date(dateFormat).toDateString()
+    );
+    
+    return res.json(statement);
 });
 
-app.delete("/courses/:id", (req, res) => {
-    return res.json([ "Curso Alterado", "Curso Patch", "Curso 3" ])
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
+    const { description, amount } = req.body;
+
+    const { customer } = req;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return res.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
+    const { amount } = req.body;
+    const { customer } = req;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount) {
+        return res.status(400).json({error: "Insufficient funds!"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return res.status(201).send();
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (req, res) => {
+    const { name } = req.body;
+    const { customer } = req;
+
+    customer.name = name;
+
+    return res.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req;
+
+    return res.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req;
+
+    customers.splice(customer, 1);
+
+    return res.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req;
+
+    const balance = getBalance(customer.statement);
+
+    return res.json(balance);
 });
